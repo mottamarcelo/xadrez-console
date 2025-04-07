9079fe33739f570b4f69c6a892c3b40558354763
Marcelo Motta <marcelorodriguesmotta@gmail.com>
2025-04-07 20:58:41 -0300

    Implementando a logica de xequemate

--- Program.cs
+++ Program.cs
@@ -43,6 +43,8 @@ namespace xadrez_console
                     }
 
                 }
+                Console.Clear();
+                Tela.imprimirPartida(partida);
 
             }
             catch (TabuleiroException e)
